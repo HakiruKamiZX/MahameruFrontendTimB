749585d8f1a0c0ddafa89c00d1f738d10e707222
Fahreza Dhia <97860152+DhiaReza@users.noreply.github.com>
2022-11-30 09:46:28 +0700

    update index.ts

--- Realization/Channel Realization/src/router/index.ts
+++ Realization/Channel Realization/src/router/index.ts
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
 import HomePage from '../views/Channel.vue'
 import InsideChann from '../views/Inside.vue'
+import ChannelInfo from '../views/Info.vue'
+import ChannelRegis from '../views/Register.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -17,7 +19,18 @@ const routes: Array<RouteRecordRaw> = [
     path: '/Inside',
     name: 'inside channel',
     component: InsideChann
+  },
+  {
+    path: '/Info',
+    name: 'Info Channel',
+    component: ChannelInfo
+  },
+  {
+    path: '/Register',
+    name: 'Register Channel',
+    component: ChannelRegis
   }
+
 ]
 
 const router = createRouter({
