243eb5d68c66cab3ef65a88608502c8f973419c1
Fahreza <fahrezadhia16@gmail.com>
2022-12-19 19:45:17 +0700

    update chat files

--- Realization/Inbox and Chat Realization/src/views/ViewChatPage.vue
+++ Realization/Inbox and Chat Realization/src/views/ViewChatPage.vue
@@ -13,26 +13,67 @@
             </ion-title>
         </ion-toolbar>
     </ion-header>
-    <ion-footer collapse="fade">
+    <ion-content class="ion-padding">
+        <ion-list lines="none">
+            <ion-item>
+                <ion-label class="to">Dah ada Kelompok belom lu ?</ion-label>
+            </ion-item>
+            <ion-item>
+                <ion-label class="to">Kalo belom bareng gw mw?<br>asdasdadsa</ion-label>
+            </ion-item>
+            <ion-item>
+                <ion-label class="from">Ntar dlu</ion-label>
+            </ion-item>
+        </ion-list>
+    </ion-content>
+    <ion-footer>
         <ion-toolbar>
-            <ion-title>Footer</ion-title>
+            <ion-title>
+                <ion-item>
+                    <ion-label position="floating">Default input</ion-label>
+                    <ion-input placeholder="Enter text"></ion-input>
+                </ion-item>
+            </ion-title>
         </ion-toolbar>
     </ion-footer>
 </template>
   
 <script lang="ts">
-import { IonHeader, IonToolbar, IonButtons, IonBackButton, IonTitle, IonFooter } from '@ionic/vue';
+import { IonHeader, IonToolbar, IonButtons, IonBackButton, IonTitle, IonFooter, IonItem, IonInput } from '@ionic/vue';
 import { defineComponent } from 'vue';
 
 export default defineComponent({
     name: 'ViewChat',
     components: {
-        IonHeader, IonToolbar, IonButtons, IonBackButton, IonTitle, IonFooter
+        IonHeader, IonToolbar, IonButtons, IonBackButton, IonTitle, IonFooter, IonItem, IonInput
     }
 });
 </script>
   
-<style>
+<style scoped>
+ion-item {
+    font-family: 'Manjari';
+    font-size: small;
+}
+
+.from {
+    border-radius: 5px;
+    border: 1px solid;
+    padding: 5px;
+}
+
+.to {
+    text-align: left;
+    border-color: #ff884b;
+    border-radius: 5px;
+    border: 1px solid;
+    padding: 5px;
+}
+
+.to {
+    text-align: right;
+}
+
 ion-toolbar {
     --background: #ff884b;
 }
